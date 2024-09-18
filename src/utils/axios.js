73f5ecb5a7ebe945b6c2dd8ac8f3cc44dbc64446
Codingmonk-yt <codingmonk22@gmail.com>
2024-09-18 14:28:24 +0530

    updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,6 +5,8 @@ const BASE_URL = 'https://trendryol-api-production.up.railway.app';
 
 // const BASE_URL = 'http://localhost:8000';
 
+
+
 // ----------------------------------------------------------------------
 
 const axiosInstance = axios.create({ baseURL: BASE_URL });
