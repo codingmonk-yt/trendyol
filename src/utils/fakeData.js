4025653ca18d3e891caed4ead1f3e38c26929c8f
Codingmonk-yt <codingmonk22@gmail.com>
2024-09-30 20:51:32 +0530

    updated

--- src/utils/fakeData.js
+++ src/utils/fakeData.js
@@ -1,51 +1,42 @@
-// const generateFakeData = () => {
-//   const fakeData = [];
-
-//   for (let i = 0; i < 10; i++) {
-//     // Generate a random Turkish mobile number (starts with 05 and has 11 digits)
-//     const mobileNumber = `05${Math.floor(100 + Math.random() * 900)}****${Math.floor(1000 + Math.random() * 9000)}`;
-    
-//     // Generate a random commission amount between 800 and 1800 TL
-//     const commission = (Math.random() * (1800 - 800) + 800).toFixed(2);
-
-//     // Create the sentence
-//     const sentence = `User[${mobileNumber}] Commission earned $${commission}`;
-
-//     // Push the sentence to the fakeData array
-//     fakeData.push(sentence);
-//   }
-
-//   // Join the array into a single string with a space between each sentence
-//   return fakeData.join(' ');
-// };
-
-// export default generateFakeData;
-
-
-
 const generateFakeData = () => {
+  const exchangeRate = 34.19; // 1 USD = 34.19 TL
   const userData = [
-    "Kullanıcı [053*****056] Komisyon Kazandı $450.00",
-    "Kullanıcı [054*****365] Komisyon Kazandı $650.00",
-    "Kullanıcı [055*****416] Komisyon Kazandı $300.00",
-    "Kullanıcı [053*****843] Komisyon Kazandı $350.00",
-    "Kullanıcı [054*****888] Komisyon Kazandı $1750.00",
-    "Kullanıcı [054*****475] Komisyon Kazandı $750.00",
-    "Kullanıcı [053*****566] Komisyon Kazandı $300.00",
-    "Kullanıcı [053*****056] Komisyon Kazandı $750.00",
-    "Kullanıcı [054*****756] Komisyon Kazandı $2350.00",
-    "Kullanıcı [055*****385] Komisyon Kazandı $650.00",
-    "Kullanıcı [050*****586] Komisyon Kazandı $300.00",
-    "Kullanıcı [055*****832] Komisyon Kazandı $1500.00",
-    "Kullanıcı [053*****206] Komisyon Kazandı $750.00",
-    "Kullanıcı [054*****365] Komisyon Kazandı $650.00",
-    "Kullanıcı [055*****456] Komisyon Kazandı $700.00",
-    "Kullanıcı [053*****052] Komisyon Kazandı $450.00"
+    "Kullanıcı [053*****056] Komisyon Kazandı TL" +
+      (450.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [054*****365] Komisyon Kazandı TL" +
+      (650.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [055*****416] Komisyon Kazandı TL" +
+      (300.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [053*****843] Komisyon Kazandı TL" +
+      (350.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [054*****888] Komisyon Kazandı TL" +
+      (1750.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [054*****475] Komisyon Kazandı TL" +
+      (750.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [053*****566] Komisyon Kazandı TL" +
+      (300.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [053*****056] Komisyon Kazandı TL" +
+      (750.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [054*****756] Komisyon Kazandı TL" +
+      (2350.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [055*****385] Komisyon Kazandı TL" +
+      (650.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [050*****586] Komisyon Kazandı TL" +
+      (300.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [055*****832] Komisyon Kazandı TL" +
+      (1500.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [053*****206] Komisyon Kazandı TL" +
+      (750.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [054*****365] Komisyon Kazandı TL" +
+      (650.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [055*****456] Komisyon Kazandı TL" +
+      (700.0 * exchangeRate).toFixed(2),
+    "Kullanıcı [053*****052] Komisyon Kazandı TL" +
+      (450.0 * exchangeRate).toFixed(2),
   ];
 
   // Join the array into a single string with a space between each sentence
-  return userData.join(' ');
+  return userData.join(" ");
 };
 
 export default generateFakeData;
-
